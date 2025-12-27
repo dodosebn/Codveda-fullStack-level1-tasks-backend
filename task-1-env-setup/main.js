a8b03f5e5c2c711d498b4485d635abd42ffeda5b
dodosebn <orjidominion32@gmail.com>
2025-12-27 18:37:28 +0100

    chnaged app to server

--- task-1-env-setup/main.js
+++ task-1-env-setup/main.js
@@ -1,22 +1,20 @@
 import express from "express";
-import dotenv from "dotenv";
-
+import dotenv from 'dotenv';
 import pkg from "pg";
 const { Client } = pkg;
 import cors from "cors";
-// const dotenv = require('dotenv');
-dotenv.config({ path: "./config.env" });
+dotenv.config({path: './config.env' })
 // whatever variable we've saved in the config.env will be saved in node js environment
 export const app = express();
 app.use(express.json());
 app.use(cors());
 
 export const db = new Client({
-  host: process.env.NODE_HOST,
+  host: "localhost",
   user: process.env.NODE_USER,
   password: process.env.NODE_PASSWORD,
   database: process.env.NODE_DATABASE,
-  port: process.env.NODE_PORT
+  port: 5000
 });
 
 
