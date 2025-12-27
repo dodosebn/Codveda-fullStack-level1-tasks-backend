b4eea08d4aaa097511b71129ba06958d88294782
dodosebn <orjidominion32@gmail.com>
2025-12-27 17:36:46 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+import { app, db } from "./task-1-env-setup/main.js";
+db.connect();
+
+app.post("/postTodos", async (req, res) => {
+  const { title } = req.body;
+  const result = await db.query(
+    "INSERT INTO todos (title) VALUES ($1) RETURNING *",
+    [title]
+  );
+  res.status(201).json(result.rows[0]);
+});
+
+app.get("/todos", async (req, res) => {
+  const result = await db.query("SELECT * FROM todos");
+  res.json(result.rows);
+});
+
+app.get("/todos/:id", async (req, res) => {
+  const result = await db.query("SELECT * FROM todos WHERE id=$1", [
+    req.params.id,
+  ]);
+  res.json(result.rows[0]);
+});
+
+app.put("/todos/:id", async (req, res) => {
+  const { completed } = req.body;
+  await db.query("UPDATE todos SET completed=$1 WHERE id=$2", [
+    completed,
+    req.params.id,
+  ]);
+  res.send("UPDATED");
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  await db.query("DELETE FROM todos WHERE id=$1", [req.params.id]);
+  res.send("DELETED");
+});
+
+app.listen(3000, () => {
+  console.log("Todo API running on port 3000");
+});
